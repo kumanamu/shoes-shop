acf44c1b0026963e7de5614bb6b1d294083bdf03
kumanamu <serachstock1@gmail.com>
2025-09-16 16:40:14 +0900

    리엑트 라우터 dom설치 및 셋팅

--- src/AppNavBar/index.jsx
+++ src/AppNavBar/index.jsx
@@ -1,6 +1,10 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Nav, Navbar } from 'react-bootstrap';
 import './AppNavBar.css'
+import { Route, Routes, Link } from 'react-router-dom';
+//route 페이지 이동 처리
+//routes uri에 담겨있는 정보를 획득
+//link  실제로 페이지를 이동하는 역할 link위치에 컴포넌트 뿌려줌
 
 function AppNavBar(){
 return(
@@ -10,12 +14,27 @@ return(
           <Container>
             <Navbar.Brand href="#home">Navbar</Navbar.Brand>
             <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#features">Features</Nav.Link>
-              <Nav.Link href="#pricing">Pricing</Nav.Link>
+              <Nav.Link> 
+                <Link to={"/"}>HOME</Link>
+                </Nav.Link>
+              <Nav.Link>
+                <Link to={"detail"}>상세페이지</Link>
+                
+                </Nav.Link>
+              <Nav.Link> 
+                <Link to ={"cart"}>장바구니페이지</Link>
+                </Nav.Link>
             </Nav>
           </Container>
         </Navbar>
+        {/* 라우팅 정보를 한꺼번에 모아놓는 장소 */}
+                {/* 스프리에서 사용하는 컨트롤러 클래스 */}
+
+        <Routes>
+          <Route path='/' element={<div>메인페이지</div>}/>
+             <Route path='/detail' element={<div>상세페이지</div>}></Route>
+                <Route path='/cart' element={<div>장바구니페이지</div>}></Route>
+        </Routes>
 
       </div>
 
